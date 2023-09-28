92af591d0cfd3b95ce99693793f0020da00947d8
Radek státní podnik <eidam30procent@gmail.com>
2023-09-28 20:39:44 +0200

    triangle shape changed

--- src/Triangle.h
+++ src/Triangle.h
@@ -15,7 +15,7 @@ private:
 static inline std::vector<Vertex> triangleData = {
 
         {.pos = {-0.15f, -0.15f, 0.f},  .color={1, 0, 0},   .uv={0,0}},
-        {.pos = {0.0f, 0.15f, 0.f},     .color={1, 0, 0},   .uv={0,1}},
+        {.pos = {-0.15f, 0.15f, 0.f},     .color={1, 0, 0}, .uv={0,1}},
         {.pos = {0.15f, -0.15f, 0.f},   .color={1, 0, 0},   .uv={1,0}},
     };
 };
